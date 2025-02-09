fd072d0d6767babac6570acda9f0eb04e3d754cc
mariomerco <mario.mercado.c@gmail.com>
2025-02-09 14:35:25 -0500

    Adding the App Source Code

---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"main/psmath"
+)
+
+func main() {
+	result := psmath.Add(2, 3)
+	fmt.Printf("The sum of 2 and 3 is %d\n", result)
+}
---
+++ app/psmath/add.go
@@ -0,0 +1,5 @@
+package psmath
+
+func Add(a, b int) int {
+	return a + b
+}
